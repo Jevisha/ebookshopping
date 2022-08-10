9c0f78ac53908c69e859a8b9f0988192b0ed3bc8
hp <hp@LAPTOP-NDAHA8SK>
2022-08-10 13:40:41 +0530

    Add project files.

---
+++ ebookshopping/Data/ebookshoppingContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ebookshopping.Models;
+
+namespace ebookshopping.Data
+{
+    public class ebookshoppingContext : DbContext
+    {
+        public ebookshoppingContext(DbContextOptions<ebookshoppingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ebookshopping.Models.book>? book { get; set; }
+
+        public DbSet<ebookshopping.Models.usersaccounts>? usersaccounts { get; set; }
+
+        public DbSet<ebookshopping.Models.orders>? orders { get; set; }
+        public DbSet<ebookshopping.Models.report>? report { get; set; }
+    }
+}
---
+++ ebookshopping/Models/book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace ebookshopping.Models
+{
+    public class book
+    {
+        public int Id { get; set; }
+        [DisplayName("Title")]
+        public string title { get; set; }
+        [DisplayName("Info")]
+        public string info { get; set; }
+        [DisplayName("Quantity")]
+        public int bookquantity { get; set; }
+        [DisplayName("Price")]
+        public int price { get; set; }
+        [DisplayName("Catid")]
+        public int cataid { get; set; }
+        [DisplayName("Author")]
+        public string author { get; set; }
+        [DisplayName("Imgfile")]
+        public string imgfile { get; set; }
+
+
+    }
+}
